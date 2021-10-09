33248837ac96535aac118eaee01ac4b9fdf6baf0
Zeno <zeno@sevencubed.tech>
2021-10-08 22:29:30 -0400

    Hello World!

---
+++ src/bindings.h
@@ -0,0 +1,6 @@
+//bindgen --use-core --no-prepend-enum-name --ctypes-prefix=ctypes --with-derive-default --no-debug \(.*\)? --blocklist-item FP_NAN --blocklist-item FP_ZERO --blocklist-item FP_INFINITE --blocklist-item FP_SUBNORMAL --blocklist-item FP_NORMAL src/bindings.h -o src/bindings.rs
+
+#include <sys/time.h>
+#include <SDL2/SDL.h>
+#define GL_GLEXT_PROTOTYPES
+#include <SDL2/SDL_opengl.h>
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![no_std]
+#![allow(nonstandard_style)]
+#![allow(improper_ctypes)]
+
+#[allow(dead_code)]
+mod ctypes;
+
+include!("bindings.rs");
